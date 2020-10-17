84d1c89b5f6c764f66202c4273b31c1c4fe899fb
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-17 13:44:58 +0300

    Update generate-c.py

--- generate-c.py
+++ generate-c.py
@@ -1,1 +1,20 @@
+print("Generate C 1.0")
+print("Use 'make shellcode' to generate shellcode.")
+shellcode = input('Enter generated shellcode: ')
 
+code = ""
+
+code += "#include<stdio.h>\n"
+code += "#include<string.h>\n"
+code += "\n"
+code += "unsigned char shellcode[] = \""+shellcode+"\";\n"
+code += "\n"
+code += "int main()\n"
+code += "{\n"
+code += "    int (*ret)() = (int(*)())shellcode;\n"
+code += "    ret();\n"
+code += "}\n"
+
+file = open("exploit.c", "a")
+file.write(code)
+file.close()
